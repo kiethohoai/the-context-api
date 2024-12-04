15b247e449492e6bfd3aa647b013bdde3e77d8c9
David Ho <kietho.060193>
2024-12-04 16:04:30 +0700

    009 useCallback in Practice

--- src/App-memo.js
+++ src/App-memo.js
@@ -1,4 +1,4 @@
-import { memo, useEffect, useMemo, useState } from 'react';
+import { memo, useCallback, useEffect, useMemo, useState } from 'react';
 import { faker } from '@faker-js/faker';
 
 function createRandomPost() {
@@ -21,9 +21,10 @@ function App() {
         )
       : posts;
 
-  function handleAddPost(post) {
+  // todo handleAddPost
+  const handleAddPost = useCallback(function handleAddPost(post) {
     setPosts((posts) => [post, ...posts]);
-  }
+  }, []);
 
   function handleClearPosts() {
     setPosts([]);
@@ -61,7 +62,11 @@ function App() {
         setSearchQuery={setSearchQuery}
       />
       <Main posts={searchedPosts} onAddPost={handleAddPost} />
-      <Archive archiveOption={archiveOption} />
+      <Archive
+        archiveOption={archiveOption}
+        onAddPost={handleAddPost}
+        setIsFakeDark={setIsFakeDark}
+      />
       <Footer />
     </section>
   );
@@ -147,7 +152,7 @@ function List({ posts }) {
   );
 }
 
-const Archive = memo(function Archive({ archiveOption }) {
+const Archive = memo(function Archive({ archiveOption, onAddPost, setIsFakeDark }) {
   // Here we don't need the setter function. We're only using state to store these posts because the callback function passed into useState (which generates the posts) is only called once, on the initial render. So we use this trick as an optimization technique, because if we just used a regular variable, these posts would be re-created on every render. We could also move the posts outside the components, but I wanted to show you this trick 😉
   const [posts] = useState(() =>
     // 💥 WARNING: This might make your computer slow! Try a smaller `length` first
@@ -170,7 +175,7 @@ const Archive = memo(function Archive({ archiveOption }) {
               <p>
                 <strong>{post.title}:</strong> {post.body}
               </p>
-              {/* <button onClick={() => onAddPost(post)}>Add as new post</button> */}
+              <button onClick={() => onAddPost(post)}>Add as new post</button>
             </li>
           ))}
         </ul>
